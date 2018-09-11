29312fec61df88c6788acdf9d7107b2acf575c04
RonchiTech <42366319+RonchiTech@users.noreply.github.com>
2018-09-11 10:49:39 +0800

    Add files via upload

--- Read/public/index.js
+++ Read/public/index.js
@@ -5,10 +5,33 @@ function login(){
       app(user);
     } else{
       var provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithRedirect(provider);
+    firebase.auth().signInWithRedirect(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
     }
   }
       firebase.auth().onAuthStateChanged(newLoginHappened);
+
+  }
+  function logout(){
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }).catch(function(error) {
+      // An error happened.
+    });
   }
 
 function app(user){
