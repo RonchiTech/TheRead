54d7582046106fab29bcf14094f171a6be810272
RonchiTech <42366319+RonchiTech@users.noreply.github.com>
2018-09-12 21:22:27 +0800

    Add files via upload

--- Read/public/index.js
+++ Read/public/index.js
@@ -5,17 +5,41 @@ function login(){
       app(user);
     } else{
       var provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithRedirect(provider);
+    firebase.auth().signInWithRedirect(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
     }
   }
       firebase.auth().onAuthStateChanged(newLoginHappened);
+
+  }
+  function logout(){
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }).catch(function(error) {
+      // An error happened.
+    });
   }
 
 function app(user){
   //user.displayName
   //user.email
   //user.uid
-
+  // var user_roles = document.getElementById('clientStatus');
+  // user_roles.style.display = 'none';
   function updateMyStatus(e){
     var myUpdate={};
     myUpdate.email = user.email;
@@ -24,11 +48,11 @@ function app(user){
     fb.child(user.uid).set(myUpdate);
 
   }
-  // function receiveUpdate(received){
-  //   var data = received.val();
-  //   console.log( data );
-  //   document.getElementById('role').innerHTML= "";
-  // }
+  function receiveUpdate(received){
+    var data = received.val();
+    console.log( data );
+    document.getElementById('role').innerHTML= "";
+  }
   document.getElementById('userName').innerHTML = user.displayName;
   var fb = firebase.database().ref('Accounts');
   document.getElementById("clientStatus").addEventListener("input",updateMyStatus);
