d687a796ca16f7e560eba291eaeaa9000caf1278
RonchiTech <42366319+RonchiTech@users.noreply.github.com>
2018-09-29 13:34:29 +0800

    Add files via upload

--- Read/public/classroom.js
+++ Read/public/classroom.js
@@ -1,38 +1,189 @@
-const classRoomData = {
-  BSIT: {
-    classRoomId: 1,
-    numberOfStudent: 5
-  },
-  BSCS: {
-    classRoomId: 2,
-    numberOfStudent: 7
-  },
-  BSCE: {
-    classRoomId: 3,
-    numberOfStudent: 8
+function login(){
+    function newLoginHappened(user){
+        if(user){
+          app(user);
+        } else{
+          var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithRedirect(provider).then(function(result){
+              // This gives you a Google Access Token. You can use it to access the Google API.
+              var token = result.credential.accessToken;
+
+              // The signed-in user info.
+              var user = result.user;
+
+            }).catch(function(error){
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              // The email of the user's account used.
+              var email = error.email;
+              // The firebase.auth.AuthCredential type that was used.
+              var credential = error.credential;
+            })
+        }
+    }
+
+    firebase.auth().onAuthStateChanged(newLoginHappened);
+}
+
+  function logout(){
+      firebase.auth().signOut().then(function(){
+        // Sign-out successful.
+      }).catch(function(error) {
+          // An error happened.
+      })
+
   }
-};
 
-const showClassData = id => {
-  document.getElementById("classInfo").innerHTML = `<div>${id}</div><div>${
-    classRoomData[id].classRoomId
-  }</div><div>${classRoomData[id].numberOfStudent}</div>`;
-};
+  function app(user){
+    document.getElementById("IDss").style.display="none";
+    document.getElementById("teacher_btn").style.display="none";
+    document.getElementById("stud_btn").style.display="none";
+    document.getElementById("classroomName").style.display = "none";
+    document.getElementById("classroomID").style.display = "none";
+    document.getElementById("ListClass").style.display = "none";
+    document.getElementById('ListClass').innerHTML = "Classroom:";
+    document.getElementById('homeName').innerHTML = user.displayName;
+    document.getElementById('homeID').innerHTML = user.uid;
+    document.getElementById("homeRole").innerHTML = user.status;
 
-const infoDiv = document.createElement("div");
-infoDiv.id = "classInfo";
+    var classuid = generateId();
+    var useROLE = document.getElementById("homeRole");
+    var fbclass = firebase.database().ref().child('Classes');
+    var fbstat = firebase.database().ref().child('Accounts/' + user.uid +'/status');
+    fbstat.on('value',function(datasnapshot){
+    homeRole.innerHTML = datasnapshot.val();
 
-const classList = document.createElement("ul");
-classList.onclick = e => {
-  showClassData(e.target.id);
-};
+      if(homeRole.innerHTML == "Teacher"){
+        document.getElementById("IDss").style.display="none";
+        document.getElementById("teacher_btn").style.display="initial";
+        document.getElementById("stud_btn").style.display="none";
+        document.getElementById('jobRole').innerHTML = "Create Classroom";
+        document.getElementById("classroomName").style.display = "initial";
+        document.getElementById("classroomID").style.display = "none";
+        document.getElementById("ListClass").style.display = "initial";
+        document.getElementById("creat_classBTN").addEventListener("click",classcreation);
+      }else if(homeRole.innerHTML == "Student"){
+        document.getElementById("IDss").style.display="initial";
+        document.getElementById("teacher_btn").style.display="none";
+        document.getElementById("stud_btn").style.display="initial";
+        document.getElementById('jobRole').innerHTML = "Add Classroom";
+        document.getElementById("classroomName").style.display = "none";
+        document.getElementById("classroomID").style.display = "initial";
+        document.getElementById("ListClass").style.display = "initial";
+        document.getElementById("add_classBTN").addEventListener("click",addclass);
+      }else{
+          window.location.href = 'index.html';
+      }
 
-Object.keys(classRoomData).forEach(classId => {
-  const classItem = document.createElement("li");
-  classItem.id = classId;
-  classItem.innerHTML = classId;
-  classList.appendChild(classItem);
-});
+  })
 
-document.body.appendChild(classList);
-document.body.appendChild(infoDiv);
+  function addclass(){
+    if (document.getElementById("classroomID").value == null && document.getElementById("classroomID").value =="" && document.getElementById("teachID").value == null && document.getElementById("teachID").value == "" ){
+         window.alert("Empty Input Field!");
+    }else{
+      var addclassID = document.getElementById("classroomID").value;
+      var teacherUID =document.getElementById("teachID").value;
+      var studentUID = user.uid;
+      var studentName = user.displayName;
+      var classRef = firebase.database().ref().child('Classes').child(teacherUID);
+      classRef.orderByChild("ClassID").equalTo(addclassID).once("child_added", function(snapshot) {
+      var studentsRef = snapshot.ref.child("MyStudents");
+      studentsRef.child(studentUID).set({ Studentname: studentName });
+    })
+
+  }
+  document.getElementById("classroomID").value = "";
+  document.getElementById("teachID").value = "";
+
+}
+  function classcreation(q){
+      if(document.getElementById('classroomName').value == ""  &&  document.getElementById('classroomName').value== null){
+          window.alert("Empty Class Name!!");
+      }else{
+        var usuid = generateId();
+        var myClasses={};
+        myClasses.TheClass = document.getElementById('classroomName').value;
+        myClasses.Teacher = user.displayName;
+        myClasses.TeacherID = user.uid;
+        myClasses.ClassID = usuid;
+        fbclass.child(user.uid).push().set(myClasses);
+      }
+      document.getElementById('classroomName').value = "";
+
+  }
+    var userRef = firebase.database().ref().child('Classes' + '/' + user.uid);
+    userRef.on('child_added', function(data){
+
+      var roomNames = data.val().TheClass;
+      var Studentx = data.val().MyStudents;
+
+      var studentRawList = '';
+      for (var key in Studentx) {
+          studentRawList += ('['+Studentx[key].Studentname + ']');
+      }
+
+      var classD = data.val().ClassID;
+      var ul = document.createElement('ul');
+      document.getElementById('myList').appendChild(ul);
+      var li = document.createElement('li');
+      ul.appendChild(li);
+
+          Object.keys(roomNames).forEach(function(key){
+            li.innerHTML += '<span onclick="clickDone(this)">'+roomNames[key]+'</span><ul style="display:none"><li>Class Id : '+classD+'</li><li><span onclick="clickDone(this)">Students :</span><ul style="display:none"><li>'+studentRawList+'</li></ul></li></ul>';
+          })
+        })
+
+        var studentRef = firebase.database().ref().child('Classes' + '/' + user.uid);
+          studentRef.on('child_added', function(data){
+
+            var roomNamess = data.val().TheClass;
+            var Studentxx = data.val().MyStudents;
+
+            var studentRawLists = '';
+            for (var keys in Studentxx) {
+              if(studentxx[keys].Studentname  == user.displayName){
+                studentRawLists += ('['+Studentxx[keys].Studentname + ']');
+
+            }
+          }
+          var classDD = data.val().ClassID;
+          var ull = document.createElement('ul');
+          document.getElementById('myLista').appendChild(ull);
+          var lii = document.createElement('li');
+          ull.appendChild(lii);
+
+          Object.keys(roomNamess).forEach(function(key){
+          lii.innerHTML += "hi";
+            })
+
+          })
+
+          function errData(err)
+           {
+              console.log('Error!');
+              console.log(err);
+
+            }
+
+            function generateId()
+            {
+              return '' + Math.random().toString(36).substr(2, 7);
+            }
+
+            function reload_page()
+            {
+              window.location.reload();
+            }
+    }
+
+    window.onload = login;
+
+      function clickDone(thisVar){
+          var is = thisVar.nextSibling.style.display;
+            if(is == 'none'){
+                thisVar.nextSibling.style.display = 'initial';
+            }else{
+                thisVar.nextSibling.style.display = 'none';
+            }
+      }
--- Read/public/home.js
+++ Read/public/home.js
@@ -1,157 +1,351 @@
 function login(){
-  function newLoginHappened(user) {
-    if(user){
-      //User is signed in
-      app(user);
-    } else{
-      var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithRedirect(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+    function newLoginHappened(user){
+        if(user){
+          app(user);
+        } else{
+          var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithRedirect(provider).then(function(result){
+              // This gives you a Google Access Token. You can use it to access the Google API.
+              var token = result.credential.accessToken;
+
+              // The signed-in user info.
+              var user = result.user;
+
+            }).catch(function(error){
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              // The email of the user's account used.
+              var email = error.email;
+              // The firebase.auth.AuthCredential type that was used.
+              var credential = error.credential;
+            })
+        }
     }
-  }
-      firebase.auth().onAuthStateChanged(newLoginHappened);
 
-  }
+    firebase.auth().onAuthStateChanged(newLoginHappened);
+}
+
   function logout(){
-    firebase.auth().signOut().then(function() {
-      // Sign-out successful.
-    }).catch(function(error) {
-      // An error happened.
-    });
+      firebase.auth().signOut().then(function(){
+        // Sign-out successful.
+      }).catch(function(error) {
+          // An error happened.
+      })
+
   }
 
-function app(user){
-  //user.displayName
-  //user.email
-  //user.uid
-  // var user_roles = document.getElementById('clientStatus');
-  // user_roles.style.display = 'none';
-  // document.getElementById('role')
-//   var userId = firebase.auth().currentUser.uid;
-//   return firebase.database().ref('/Accounts/' + userId).on('value').then(function(snapshot) {
-//   var statuz = (snapshot.val() && snapshot.val().status);
-// });
-  // document.getElementById("useROLE").innerHTML = user.status;
-  // document.getElementById("createClassRoom").style.display = "none";
+  function app(user){
+    document.getElementById("IDss").style.display="none";
+    document.getElementById("teacher_btn").style.display="none";
+    document.getElementById("stud_btn").style.display="none";
+    document.getElementById("classroomName").style.display = "none";
+    document.getElementById("classroomID").style.display = "none";
+    document.getElementById("ListClass").style.display = "none";
+    document.getElementById('ListClass').innerHTML = "Classroom:";
+    document.getElementById('homeName').innerHTML = user.displayName;
+    document.getElementById('homeID').innerHTML = user.uid;
+    document.getElementById("homeRole").innerHTML = user.status;
 
-  // document.getElementById("createClassRoom").style.display = "none";
-  document.getElementById("teacher_btn").style.display="none";
-  document.getElementById("classroomName").style.display = "none";
-  document.getElementById("ListClass").style.display = "none";
-  document.getElementById('ListClass').innerHTML = "Classroom:";
-  document.getElementById('homeName').innerHTML = user.displayName;
-  document.getElementById("homeRole").innerHTML = user.status;
-  var classuid = generateId();
-  var useROLE = document.getElementById("homeRole");
-  var fbclass = firebase.database().ref().child('Classes');
-  var fbstat = firebase.database().ref().child('Accounts/' + user.uid +'/status');
-  fbstat.on('value',function(datasnapshot){
+    var classuid = generateId();
+    var useROLE = document.getElementById("homeRole");
+    var fbclass = firebase.database().ref().child('Classes');
+    var fbstat = firebase.database().ref().child('Accounts/' + user.uid +'/status');
+    fbstat.on('value',function(datasnapshot){
     homeRole.innerHTML = datasnapshot.val();
 
-if(homeRole.innerHTML == "Teacher"){
-  document.getElementById("teacher_btn").style.display="initial";
-  document.getElementById('jobRole').innerHTML = "Create Classroom";
-  document.getElementById("classroomName").style.display = "initial";
-  document.getElementById("ListClass").style.display = "initial";
-  document.getElementById("creat_classBTN").addEventListener("click",classcreation);
-
-  // document.getElementById("createClassRoom").style.display = "initial";
-} else if(homeRole.innerHTML == "" && homeRole.innerHTML ==null) {
-  window.location.href = 'index.html';
-}   else {
-  document.getElementById('jobRole').innerHTML = "View Classroom";
-}
-
-  });
+      if(homeRole.innerHTML == "Teacher"){
+        document.getElementById("IDss").style.display="none";
+        document.getElementById("teacher_btn").style.display="initial";
+        document.getElementById("stud_btn").style.display="none";
+        document.getElementById('jobRole').innerHTML = "Create Classroom";
+        document.getElementById("classroomName").style.display = "initial";
+        document.getElementById("classroomID").style.display = "none";
+        document.getElementById("ListClass").style.display = "initial";
+        document.getElementById("creat_classBTN").addEventListener("click",classcreation);
 
 
+        var userRef = firebase.database().ref().child('Classes' + '/' + user.uid);
+        userRef.on('child_added', function(data){
+
+          var roomNames = data.val().TheClass;
+          var Studentx = data.val().MyStudents;
+
+          var studentRawList = '';
+          for (var key in Studentx) {
+              studentRawList += ('['+Studentx[key].Studentname + ']');
+          }
+
+          var classD = data.val().ClassID;
+          var ul = document.createElement('ul');
+          document.getElementById('myList').appendChild(ul);
+          var li = document.createElement('li');
+          ul.appendChild(li);
+
+              Object.keys(roomNames).forEach(function(key){
+                li.innerHTML += '<span onclick="clickDone(this)">'+roomNames[key]+'</span><ul style="display:none"><li>Class Id : '+classD+'</li><li><span onclick="clickDone(this)">Students :</span><ul style="display:none"><li>'+studentRawList+'</li></ul></li></ul>';
+              })
+                })
+              // document.getElementById("myLista").innerHTML = "Teacher here";
+
+      }else if(homeRole.innerHTML == "Student"){
+        document.getElementById("IDss").style.display="initial";
+        document.getElementById("teacher_btn").style.display="none";
+        document.getElementById("stud_btn").style.display="initial";
+        document.getElementById('jobRole').innerHTML = "Add Classroom";
+        document.getElementById("classroomName").style.display = "none";
+        document.getElementById("classroomID").style.display = "initial";
+        document.getElementById("ListClass").style.display = "initial";
+        document.getElementById("add_classBTN").addEventListener("click",addclass);
 
 
-  function classcreation(q) {
-    var checkcn = document.getElementById('classroomName').value;
-    if(checkcn == "" && checkcn == null){
-      alert("Empty Class Name!!");
-    }else {
-    var usuid = generateId();
-    var myClasses={};
-    myClasses.TheClass = document.getElementById('classroomName').value;
-    myClasses.Teacher = user.displayName;
-    myClasses.TeacherID = user.uid;
-    myClasses.ClassID = usuid;
-    fbclass.child(user.uid).push().set(myClasses);
-    // var xx = document.getElementById('classroomName').value;
-    // document.getElementById('ListClass').innerHTML = x;
-}
-  }
-  var userRef = firebase.database().ref().child('Classes' + '/' + user.uid);
-  userRef.on('child_added', function(data) {
 
-  var roomNames = data.val().TheClass;
-  var titsNames = data.val().Teacher;
-  var classD = data.val().ClassID;
+               var userStudent = firebase.database().ref().child('Classes/' );
+                userStudent.on('child_added', function(data){
 
-  var ul = document.createElement('ul');
+                  var ctr= 0;
+                  var displayName = user.displayName;
+                  var Studentname = data.val().Studentname;
+                  var MyStudents = data.val().MyStudents;
+                  var TheClass = data.val().TheClass;
+                                // document.getElementById("myLista").innerHTML=suid;
+                                // console.log(suid);
+                 //
+                 //
+                 // var uidd = user.uid;
+                 // console.log(uidd);
+                 // document.getElementById("myLista").innerHTML=uidd;
+                 // console.log(user.uid);
+                 // for(var i in Studentname){
+                 //   if(Studentname[i] == displayName){
+                     console.log(Studentname);
+                 //   }
+                   // console.log(ctr);
+
+                  // for(var i2 in student){
+                  //
+                  //     if(student[i2] == suid){
+                  //
+                  //       //student assigned to room
+                  //     console.log(this.data[i].val().TheClass);
+                  //
+                  //   }
+                  //
+                  // }
+
+                // }
+              })
+        // console.log(user.uid);
+        // var studentClassRef = firebase.database().ref().child('Classes' + '/' + user.uid);
+        //  studentClassRef.on('child_added', function(data){
+        //    console.log(user.uid);
+        //   //  for(i in this.data){
+        //   //
+        //   //   var uid = user.uid;
+        //   //
+        //   //   var student = this.data[i].val().Studentname;
+        //   //   console.log(uid);
+        //   //   // for(i2 in student){
+        //   //   //
+        //   //   //     if(student[i2] == uid){
+        //   //   //
+        //   //   //       //student assigned to room
+        //   //   //     // console.log(this.data[i].val().TheClass);
+        //   //   //
+        //   //   //   }
+        //   //   //
+        //   //   // }
+        //   //
+        //   // }
+        // })
+
+        // var studentClassRef = firebase.database().ref().child('Classes' + '/' + user.uid);
+        // studentClassRef.on('child_added', function(data){
+        //   var room_name = data.val().TheClass;
+        //   var room_students = data.val().MyStudents;
+        //   student_list = '';
+        //   for (var k in room_students){
+        //     student_list += (room_students[k].Studentname);
+        //   }
+        //     document.getElementById("myLista").innerHTML += student_list;
+        // })
+  //       var studentClassRef = firebase.database().ref().child('Classes' + '/' + user.uid);
+  //       studentClassRef.on('child_added', function(data){
+  //
+  //         var roomNamess = data.val().TheClass;
+  //         var Studentxx = data.val().MyStudents;
+  //         var classDD = data.val().ClassID;
+  //
+  //         var ul2 = document.createElement('ul');
+  //         document.getElementById('myLista').appendChild(ul2);
+  //         var li2 = document.createElement('li');
+  //         ul2.appendChild(li2);
+  //
+  //
+  //         var studentRawLists= '';
+  //         for (var keys in Studentxx) {
+  //           if(studentxx[keys].Studentname == user.displayName){
+  //             studentRawLists += ('['+Studentxx[keys].Studentname + ']');
+  //         }
+  //             Object.keys(roomNamess).forEach(function(keyz){
+  //               document.getElementById("myLista").innerHTML = "hi" + '<ul style="display:none"><li>' + roomNamess[keyz]; '</li></ul>';
+  //                             //'<span onclick="clickDone2(this)">'+roomNamess[keyz]+'</span><ul style="display:none"><li>Class Id : '+classDD+'</li><li><span onclick="clickDone(this)">Students :</span><ul style="display:none"><li>'+studentRawLists+'</li></ul></li></ul>';
+  //
+  //           // })
+  //
+  //
+  //       })
+  //     }
+  // })
 
 
-  document.getElementById('myList').appendChild(ul);
+        // document.getElementById("myLista").innerHTML = "Student here";
 
-
-  var li = document.createElement('li');
-  var lit = document.createElement('ulok');
-  ul.appendChild(li);
-
-
-  Object.keys(roomNames).forEach(function(key){
-       li.innerHTML += '<span onclick="clickDone(this)">'+roomNames[key]+'</span><ul style="display:none"><li>Class Id : '+classD+'</li><li>Teacher : '+titsNames+'</li></ul>';
-
-      });
-
-
-  });
-
-
-  function errData(err) {
-    console.log('Error!');
-    console.log(err);
-
-  }
-
-
-  function generateId(){
-    return 'xxxx-xxxx-xxxx'.replace(/[x]/g, function(){
-      return (Math.random() * 9 | 0 ).toString();
+      }
+      else{
+          window.location.href = 'index.html';
+      }
     })
+    // console.log(user.uid);
+    //
+    // var uidd = user.uid;
+    // var studentClassRef = firebase.database().ref().child('Classes' + '/' + user.uid);
+    //  studentClassRef.on('child_added', function(data){
+    //
+    //    var uidd = user.uid;
+    //
+    //    console.log(uidd);
+    //    // console.log(user.uid);
+    //   //  for(i in this.data){
+    //   //
+    //   //   var uid = user.uid;
+    //   //
+    //   //   var student = this.data[i].val().Studentname;
+    //   //   console.log(uid);
+    //   //   // for(i2 in student){
+    //   //   //
+    //   //   //     if(student[i2] == uid){
+    //   //   //
+    //   //   //       //student assigned to room
+    //   //   //     // console.log(this.data[i].val().TheClass);
+    //   //   //
+    //   //   //   }
+    //   //   //
+    //   //   // }
+    //   //
+    //   // }
+    // })
+    // console.log(user.uid);
+
+  function addclass(){
+    if (document.getElementById("classroomID").value == nul ||  document.getElementById("classroomID").value =="" || document.getElementById("teachID").value == null || document.getElementById("teachID").value == "" ){
+         window.alert("Empty Input Field!");
+    }else{
+      var addclassID = document.getElementById("classroomID").value;
+      var teacherUID =document.getElementById("teachID").value;
+      var studentUID = user.uid;
+      var studentName = user.displayName;
+      var classRef = firebase.database().ref().child('Classes').child(teacherUID);
+      classRef.orderByChild("ClassID").equalTo(addclassID).once("child_added", function(snapshot) {
+      var studentsRef = snapshot.ref.child("MyStudents");
+      studentsRef.child(studentUID).set({ Studentname: studentName });
+    })
+
   }
-
-  function reload_page(){
-   window.location.reload();
-  }
-
-
+  document.getElementById("classroomID").value = "";
+  document.getElementById("teachID").value = "";
 
 }
 
-window.onload = login;
 
-function clickDone(thisVar){
-  var is = thisVar.nextSibling.style.display;
-  if(is == 'none'){
-    thisVar.nextSibling.style.display = 'initial';
-  }else{
-    thisVar.nextSibling.style.display = 'none';
+  function classcreation(q){
+      if(document.getElementById('classroomName').value == ""  ||  document.getElementById('classroomName').value== null){
+          window.alert("Empty Class Name!!");
+      }else{
+        var usuid = generateId();
+        var myClasses={};
+        myClasses.TheClass = document.getElementById('classroomName').value;
+        myClasses.Teacher = user.displayName;
+        myClasses.TeacherID = user.uid;
+        myClasses.ClassID = usuid;
+        fbclass.child(user.uid).push().set(myClasses);
+
+        // var usuid = generateId();
+        // var myClasses={};
+        // myClasses.TheClass = document.getElementById('classroomName').value;
+        // myClasses.Teacher = user.displayName;
+        // myClasses.TeacherID = user.uid;
+        //  myClasses.ClassID = usuid;
+        // fbclass.child(myClasses.ClassID).set(myClasses);
+      }
+      document.getElementById('classroomName').value = "";
+
   }
-}
+
+
+
+      //
+      // var userStudent = firebase.database().ref().child('Classes' + '/' + user.uid);
+      //  userStudent.on('child_added', function(data){
+      //
+      //
+      //    var uidd = user.uid;
+      //    console.log(uidd);
+      //    document.getElementById("myLista").innerHTML=uidd;
+      //    // console.log(user.uid);
+      //   //  for(i in this.data){
+      //   //
+      //   //   var uid = user.uid;
+      //   //
+      //   //   var student = this.data[i].val().Studentname;
+      //   //   console.log(uid);
+      //   //   // for(i2 in student){
+      //   //   //
+      //   //   //     if(student[i2] == uid){
+      //   //   //
+      //   //   //       //student assigned to room
+      //   //   //     // console.log(this.data[i].val().TheClass);
+      //   //   //
+      //   //   //   }
+      //   //   //
+      //   //   // }
+      //   //
+      //   // }
+      // })
+          function errData(err)
+           {
+              console.log('Error!');
+              console.log(err);
+
+            }
+
+            function generateId(){
+              return '' + Math.random().toString(36).substr(2, 7);
+            }
+
+            function reload_page()
+            {
+              window.location.reload();
+            }
+    }
+
+    window.onload = login;
+
+      function clickDone(thisVar){
+          var is = thisVar.nextSibling.style.display;
+            if(is == 'none'){
+                thisVar.nextSibling.style.display = 'initial';
+            }else{
+                thisVar.nextSibling.style.display = 'none';
+            }
+      }
+
+
+            function clickDone2(thisVar2){
+                var is2 = thisVar.nextSibling.style.display;
+                  if(is2 == 'none'){
+                      thisVar.nextSibling.style.display = 'initial';
+                  }else{
+                      thisVar.nextSibling.style.display = 'none';
+                  }
+            }
